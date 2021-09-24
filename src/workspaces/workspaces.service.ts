cece7318a4a20eb0fa4044fe956d76cb3b199792
tasddc1226 <tasddc@naver.com>
2021-09-24 18:24:11 +0900

    [feat] create workspace, get workspaces by using QueryBuilder

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -37,6 +37,41 @@ export class WorkspacesService {
     }
 
     async createWorkspace(name: string, url: string, myId: number) {
+        const workspace = this.workspacesRepository.create({
+            name,
+            url,
+            OwnerId: myId,
+        });
+        const returned = await this.workspacesRepository.save(workspace);
+        const workspaceMember = new WorkspaceMembers();
+        workspaceMember.UserId = myId;
+        workspaceMember.WorkspaceId = returned.id;
+        const channel = new Channels();
+        channel.name = '일반';
+        channel.WorkspaceId = returned.id;
+        const [, channelReturned] = await Promise.all([
+            this.workspaceMembersRepository.save(workspaceMember),
+            this.channelsRepository.save(channel),
+        ]);
+        const channelMember = new ChannelMembers();
+        channelMember.UserId = myId;
+        channelMember.ChannelId = channelReturned.id;
+        await this.channelMembersRepository.save(channelMember);
+    }
+
+    // 워크스페이스의 멤버들을 가져오는 서비스 : 쿼리 빌더 사용
+    // 쿼리 빌더 : typeORM이 최대한 SQL과 비슷하게 만들어 놓은 것
+    async getWorkspaceMembers(url: string) {
+        this.usersRepository
+            // u는 Users entity 대한 별명
+            .createQueryBuilder('u')
+            .innerJoin('u.WorkspaceMembers', 'm')
+            // sql injection 방어를 위한 url 파싱 방법
+            .innerJoin('m.Workspace', 'w', 'w.url = :url', { url })
+            // join 결과를 객체로 가져오기 위해서 getMany() 사용
+            // 하지만 자바스크립트로 한번 가공하기 때문에 속도가 느린 단점
+            // ORM을 사용하는데 성능까지..? -> Row query 날리기
+            .getMany();
 
     }
 
