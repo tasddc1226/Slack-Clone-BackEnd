af9b33519166edfd5c0e37574bbb272b0bde1fde
tasddc1226 <tasddc@naver.com>
2021-10-12 20:45:45 +0900

    [feat] multer 모듈, @types/multer 모듈 추가, dms 컨트롤러, 서비스구현

--- src/dms/dms.module.ts
+++ src/dms/dms.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DmsService } from './dms.service';
-import { DmsController } from './dms.controller';
+import { DMsService } from './dms.service';
+import { DMsController } from './dms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DMs } from 'src/entities/DMs';
+import { Users } from 'src/entities/Users';
+import { Workspaces } from 'src/entities/Workspaces';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
-  providers: [DmsService],
-  controllers: [DmsController]
+  imports: [TypeOrmModule.forFeature([DMs, Users, Workspaces]), EventsModule],
+  providers: [DMsService],
+  controllers: [DMsController]
 })
-export class DmsModule {}
+export class DmsModule { }
